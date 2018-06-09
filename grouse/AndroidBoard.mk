ab62283ccf0ed27b2e1389f2fdbab42ecfdc4bb3
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-06-09 10:19:08 -0700

    grouse: seeding content for build.
    Change-Id: I31556a73f7573f8fe1bcb3c05fb7a242d1fc45d2
    refs: e287ff4a98cca0585d6888956a3d3876aa9c9caa
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

---
+++ grouse/AndroidBoard.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(my-dir)
+
+include $(LOCAL_PATH)/AndroidKernel.mk
+include $(LOCAL_PATH)/AndroidPhony.mk
+
+$(call add-radio-file, bootloader.dev.img)
+$(call add-radio-file, bootloader.prod.img)
+
+ifneq ($(BCM_DIST_BLIM_BINS),y)
+$(call add-radio-file, gpt.bin)
+endif
+
---
+++ grouse/BoardConfig.mk
@@ -0,0 +1,7 @@
+TARGET_USES_MKE2FS := true
+
+# temporary until we fix all props correctly b/78205669
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := false
+
+include device/broadcom/common/BoardConfig.mk
+
