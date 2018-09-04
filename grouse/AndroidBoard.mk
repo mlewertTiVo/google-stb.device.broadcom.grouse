8229bf58881cfc361f2f309b103e0d3a2c3b9e16
Jay Patel <pateljay@google.com>
2018-09-04 16:58:57 -0700

    fix gpt.bin issue in auto-build for grouse Test: build grouse Bug: 113946194
    Change-Id: I09c0e58bf83ffa39819181a7436e3eb684409a00

--- grouse/AndroidBoard.mk
+++ grouse/AndroidBoard.mk
@@ -6,7 +6,7 @@ include $(LOCAL_PATH)/AndroidPhony.mk
 $(call add-radio-file, bootloader.dev.img)
 $(call add-radio-file, bootloader.prod.img)
 
-ifneq ($(BCM_DIST_BLIM_BINS),y)
+ifneq ($(BCM_DIST_BLIMG_BINS),y)
 $(call add-radio-file, gpt.bin)
 endif
 
