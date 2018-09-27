975f2d43255383c9630a495b99c2d6d2e9908c2b
Pierre Couillaud <pierre@broadcom.com>
2018-09-27 09:58:47 -0700

    grouse: allow inherting devices to swap some settings (ii).
    refs #SWANDROID-5556
    
    Change-Id: I45dcb9cba90e7f8c1e5e758ef41eb4a78cb77804
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- grouse.mk
+++ grouse.mk
@@ -22,12 +22,12 @@ LOCAL_DEVICE_RECOVERY_RCS        ?= device/broadcom/common/rcs/init.recovery.rc:
 LOCAL_DEVICE_RECOVERY_RCS        += device/broadcom/grouse/rcs/init.block.rc:root/init.recovery.block.rc # block devices
 
 # kernel command line.
-LOCAL_DEVICE_KERNEL_CMDLINE      ?= mem=2000m@0m mem=40m@2008m
-LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=532m@414m
-LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=574m@948m
-LOCAL_DEVICE_KERNEL_CMDLINE      += ramoops.mem_address=0x7D000000 ramoops.mem_size=0x800000 ramoops.console_size=0x400000
-LOCAL_DEVICE_KERNEL_CMDLINE      += rootwait init=/init ro
-export LOCAL_DEVICE_KERNEL_CMDLINE
+KERNEL_CMDLINE      := mem=2000m@0m mem=40m@2008m
+KERNEL_CMDLINE      += bmem=532m@414m
+KERNEL_CMDLINE      += brcm_cma=574m@948m
+KERNEL_CMDLINE      += ramoops.mem_address=0x7D000000 ramoops.mem_size=0x800000 ramoops.console_size=0x400000
+KERNEL_CMDLINE      += rootwait init=/init ro
+export LOCAL_DEVICE_KERNEL_CMDLINE ?= ${KERNEL_CMDLINE}
 
 # compile the media codecs for the device.
 LOCAL_DEVICE_MEDIA               := device/broadcom/common/media/media_codecs_no_legacy_enc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
