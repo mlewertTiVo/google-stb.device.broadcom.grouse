aaa23f10cb546dac63e5235245199f4e033d5961
Patrick Pun <patrick.pun@broadcom.com>
2019-03-28 10:46:13 -0700

    Fixed grousepck build issue
    refs #SWANDROID-6306
    
    Change-Id: I5fb7631ac92ce161f138327f997ac47e30e32614
    (cherry picked from commit 53e5a7a11e654c8146d198ecec7c4356de951acf)
    (cherry picked from commit 49540573c93b094febf715bb661f008bb5553c5a)

--- grousepck.mk
+++ grousepck.mk
@@ -27,7 +27,7 @@ export LOCAL_DEVICE_KERNEL_CMDLINE
 # compile the media codecs for the device.
 LOCAL_DEVICE_MEDIA               := device/broadcom/common/media/media_codecs_no_legacy_enc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 LOCAL_DEVICE_MEDIA               += device/broadcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
-LOCAL_DEVICE_MEDIA               += device/broadcom/grouse/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+LOCAL_DEVICE_MEDIA               += device/broadcom/grouse/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 export LOCAL_DEVICE_MEDIA
 
 export HW_AB_UPDATE_SUPPORT      := y
