723b3197f539ea48bf8f02c151a1b097110b549b
Pierre Couillaud <pierre@broadcom.com>
2019-08-09 07:35:44 -0700

    grouse: "ro.build.reference.fingerprint" is a system property
    refs #SWANDROID-6823
    refs #SWANDROID-6914
    
    Change-Id: I8d840a2d9dc4195665042c5f25bba55542ef0075
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>
    (cherry picked from commit 46b604e678886f2914a8ee6c1c450d1557665df8)
    (cherry picked from commit ae298fe2a46507f710195c99d72a5fcaf22703d2)

--- reference_build.mk
+++ reference_build.mk
@@ -1,5 +1,5 @@
 # This is the build fingerprint of the reference platform that the current build is
 # based on.
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.reference.fingerprint=ANDROIDTVREFERENCEBUILDPLACEHOLDER
 
